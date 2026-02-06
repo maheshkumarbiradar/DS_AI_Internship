000f341b42ec06414c55830cd0404ea2ed1005b1
Maheshkumar Biradar <maheshkumarrbiradar67@gmail.com>
2026-02-06 13:41:34 +0530

    Day 5 task 1

--- src/day5.py
+++ src/day5.py
@@ -2,4 +2,4 @@ def calc_rectangle(length, width):
     area = length * width
     perimeter = 2*(length+width)
     return area, perimeter
-print(f"Area and Perimeter of Rectangle:",calc_rectangle(5,3))
+print(f"Area & Perimeter of Rectangle:",calc_rectangle(5,3))
